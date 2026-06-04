6e64a93090170865ee676096b96655158a65ff1b
Bakame <topdvgroup@gmail.com>
2026-06-04 20:35:25 +0200

    added the calculation of marks and made it functional

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 console.log("scripting started..");
 
-
 const computerGuess = function computerGuess() {
     computerChoice = Math.floor(Math.random()*3 + 1)
     if(computerChoice == 1) return "stone";
@@ -16,6 +15,7 @@ const userGuess = function userGuess() {
 }
 
 const decision = function decision(computerGuess,userGuess) {
+    if(userGuess == -1) return 'F'
     if(computerGuess === userGuess) return "tied";
     if(computerGuess === 'stone'&& userGuess == "scissor" ) return 'C';
     if(computerGuess === 'scissor'&& userGuess == "paper" ) return 'C';
@@ -24,10 +24,44 @@ const decision = function decision(computerGuess,userGuess) {
     return 'S'
 }
 
-_computerGuess = computerGuess()
-_userGuess = userGuess()
+let count = 0;
+let computerMarks = 0;
+let userMarks = 0;
+const ROUNDS = 5;
 
-console.log(decision(_computerGuess,_userGuess));
+const mainFunction = function mainFunction() {
+    _computerGuess = computerGuess()
+    _userGuess = userGuess()
+
+    const decide = decision(_computerGuess,_userGuess)
+    console.log( decide);
+    
+    if(decide =='C') computerMarks++;
+    if(decide =='S') userMarks++;
+
+    count ++
+
+    if(count === ROUNDS){
+        if(computerMarks===userMarks){
+            console.log("tied");
+            return
+        }else if(computerMarks>userMarks){
+            console.log("computer: " + computerMarks );
+            return
+
+        }else{
+            console.log("user: " + userMarks);
+            return
+        }
+        
+    }
+
+     mainFunction()
+   
+}
+
+mainFunction()
+
 
 
 
